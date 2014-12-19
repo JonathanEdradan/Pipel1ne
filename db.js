3f49e482265eeaf064c8a76b258d7b7d59c32cd8
Jonathan Edradan <jmedradan@gmail.com>
2014-12-19 10:16:17 -0800

    Changed db.js for heroku deployment

--- db.js
+++ db.js
@@ -3,32 +3,16 @@ var pg = require('pg');
 // Set up database
 var db = {};
 
-// Local
-db.config = {
-  database: "pipel1ne",
-  port: 5432,
-  host: "localhost"
-};
+// // Local
+// db.config = {
+//   database: "pipel1ne",
+//   port: 5432,
+//   host: "localhost"
+// };
 
-// Local
-db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
-    if (err) {
-      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
-    }
-    runAfterConnecting(client);
-    done();
-  });
-};
-
-// // Heroku DB
-// db.config = {};
-
-// // Heroku
+// // Local
 // db.connect = function(runAfterConnecting) {
-//   console.log(process.env.DATABASE_URL);
-
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done){
+//   pg.connect(db.config, function(err, client, done){
 //     if (err) {
 //       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
 //     }
@@ -36,6 +20,22 @@ db.connect = function(runAfterConnecting) {
 //     done();
 //   });
 // };
+
+// Heroku DB
+db.config = {};
+
+// Heroku
+db.connect = function(runAfterConnecting) {
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    if (err) {
+      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+    }
+    runAfterConnecting(client);
+    done();
+  });
+};
 
 db.query = function(statement, params, callback){
   db.connect(function(client){
